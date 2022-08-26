d88bd3aa717a29a4be019c9a7f6715fbbac7b2a6
lidawei <lidawei918@163.com>
2022-08-26 14:33:03 +0800

    20220826

---
+++ 一键配置LVS.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Author:wangxiaochun
+#Date:2017-08-13
+vip=10.0.0.200
+mask='255.255.255.255'
+dev=lo:1
+
+case $1 in
+start)
+    echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore
+    echo 1 > /proc/sys/net/ipv4/conf/lo/arp_ignore
+    echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce
+    echo 2 > /proc/sys/net/ipv4/conf/lo/arp_announce
+    ifconfig $dev $vip netmask $mask 
+    echo "The RS Server is Ready!"
+    ;;
+stop)
+    ifconfig $dev down
+    echo 0 > /proc/sys/net/ipv4/conf/all/arp_ignore
+    echo 0 > /proc/sys/net/ipv4/conf/lo/arp_ignore
+    echo 0 > /proc/sys/net/ipv4/conf/all/arp_announce
+    echo 0 > /proc/sys/net/ipv4/conf/lo/arp_announce
+    echo "The RS Server is Canceled!"
+    ;;
+*) 
+    echo "Usage: $(basename $0) start|stop"
+    exit 1
+    ;;
+esac
